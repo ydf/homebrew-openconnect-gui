1883c6b97552d57533cbab1d89fc5acdb2871475
admin <admin@hongmi.local>
2021-12-10 16:20:07 +0800

    使用最新的代码

--- Formula/openconnect-gui.rb
+++ Formula/openconnect-gui.rb
@@ -1,20 +1,8 @@
 class OpenconnectGui < Formula
   desc "Graphical OpenConnect VPN client"
   homepage "https://openconnect.github.io/openconnect-gui/"
-  stable do
-    url "https://github.com/openconnect/openconnect-gui/archive/v1.5.3.tar.gz"
-    # version "1.5.3"
-    # revision 1
-    sha256 "bb93486a9911b4834ab93ab4396f4719725d885ca5538cd70c21699cc49c9a8a"
-  end
-
-  bottle do
-    root_url "https://github.com/openconnect/openconnect-gui/releases/download/v1.5.3"
-    sha256 "b4e5c8618cb327cd3ba612a25976d7df7b49f612669f90488d8c680e32f8f61f" => :high_sierra
-  end
-
   head do
-    url "https://github.com/openconnect/openconnect-gui.git", :branch => "develop", :shallow => false
+    url "https://gitlab.com/openconnect/openconnect-gui.git", :branch => "develop", :shallow => false
   end
 
   option "with-ini-settings", "Store the settings in INI files"
